7cb194de1f0a798ee595c0ae080867811c7f9456
Sylvain <c84.sylvain@gmail.com>
2022-02-08 09:05:56 +0100

    All files

---
+++ connectDB.php
@@ -0,0 +1,12 @@
+<?php
+
+  require('params.php');
+
+try {
+  $bddConnect = new PDO("mysql:host=".$servername.";dbname=".$dbname, $dbuser, $dbpassword);
+  $bddConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully &#128515;";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+ ?>
